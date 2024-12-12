02e5715cd6af8d2ac1923d1ea2812a55e1e9c9b6
Ademe <A.AYAD@lprs.fr>
2024-12-12 13:58:30 +0100

    [feat] Initialisation du projet

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenue au jeu du morpion (ou tic tac toe ( ou croix rond pour les intimes))");
+        System.out.println("Le joueur 1 devra jouer ");
+    }
+}
