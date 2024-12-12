e03414b7ae9fb60400e402bd74510a48502788be
Ademe <A.AYAD@lprs.fr>
2024-12-12 16:30:52 +0100

    [feat] Possibilité de relancer la partie

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
-        System.out.println("Bienvenue au jeu du morpion (ou tic tac toe ( ou croix rond pour les intimes))");
-        System.out.println("Le joueur 1 devra jouer les 'X' et le joueur 2 les 'O'");
-        System.out.println("Il faut alligner trois mêmes symboles pour gagner la partie");
-        System.out.println("Joueur 1, veuillez entrer votre nom");
-        String jUn = clavier.nextLine();
-        System.out.println("Joueur 2, à votre tour d'entrer votre nom");
-        String jDeux = clavier.nextLine();
-        char X ="X".charAt(0);
-        char O ="O".charAt(0);
-        char[][] tableauMorpion = {
-                {'A', 'B','C'},
-                {'D', 'E', 'F'},
-                {'G', 'H', 'I'},
-        };
-        int nbTours = 9;
-        while (nbTours > 0) {
+        String reponseRelancer = "o";
+        do {
+            System.out.println("Bienvenue au jeu du morpion (ou tic tac toe ( ou croix rond pour les intimes))");
+            System.out.println("Le joueur 1 devra jouer les 'X' et le joueur 2 les 'O'");
+            System.out.println("Il faut alligner trois mêmes symboles pour gagner la partie");
+            System.out.println("Joueur 1, veuillez entrer votre nom");
+            String jUn = clavier.nextLine();
+            System.out.println("Joueur 2, à votre tour d'entrer votre nom");
+            String jDeux = clavier.nextLine();
+            char X = "X".charAt(0);
+            char O = "O".charAt(0);
+            char[][] tableauMorpion = {
+                    {'A', 'B', 'C'},
+                    {'D', 'E', 'F'},
+                    {'G', 'H', 'I'},
+            };
+            int nbTours = 9;
+            while (nbTours > 0) {
                 System.out.println(Arrays.deepToString(tableauMorpion));
                 System.out.println(jUn + ", où voulez vous placer votre X.");
                 char reponseJUn = clavier.nextLine().toUpperCase().charAt(0);
@@ -69,41 +71,41 @@ public class Main {
                         nbTours -= 1;
                         break;
                 }
-                if (tableauMorpion[0][0]==X && tableauMorpion[1][0]==X && tableauMorpion[2][0]==X) {
-                    System.out.println(Arrays.deepToString(tableauMorpion));
-                    System.out.println(jUn+" a gagné !");
-                    break;
-                } else if(tableauMorpion[0][0]==X && tableauMorpion[0][1]==X && tableauMorpion[0][2]==X) {
-                    System.out.println(Arrays.deepToString(tableauMorpion));
-                    System.out.println(jUn+" a gagné !");
-                    break;
-                }else if(tableauMorpion[1][0]==X && tableauMorpion[1][1]==X && tableauMorpion[1][2]==X) {
-                    System.out.println(Arrays.deepToString(tableauMorpion));
-                    System.out.println(jUn+" a gagné !");
-                    break;
-                }else if(tableauMorpion[2][0]==X && tableauMorpion[2][1]==X && tableauMorpion[2][2]==X) {
+                if (tableauMorpion[0][0] == X && tableauMorpion[1][0] == X && tableauMorpion[2][0] == X) {
                     System.out.println(Arrays.deepToString(tableauMorpion));
                     System.out.println(jUn + " a gagné !");
                     break;
-                } else if(tableauMorpion[0][1]==X && tableauMorpion[1][1]==X && tableauMorpion[2][1]==X) {
+                } else if (tableauMorpion[0][0] == X && tableauMorpion[0][1] == X && tableauMorpion[0][2] == X) {
                     System.out.println(Arrays.deepToString(tableauMorpion));
                     System.out.println(jUn + " a gagné !");
                     break;
-                } else if(tableauMorpion[0][2]==X && tableauMorpion[1][2]==X && tableauMorpion[2][2]==X) {
+                } else if (tableauMorpion[1][0] == X && tableauMorpion[1][1] == X && tableauMorpion[1][2] == X) {
                     System.out.println(Arrays.deepToString(tableauMorpion));
                     System.out.println(jUn + " a gagné !");
                     break;
-                }else if(tableauMorpion[0][0]==X && tableauMorpion[1][1]==X && tableauMorpion[2][2]==X) {
+                } else if (tableauMorpion[2][0] == X && tableauMorpion[2][1] == X && tableauMorpion[2][2] == X) {
                     System.out.println(Arrays.deepToString(tableauMorpion));
                     System.out.println(jUn + " a gagné !");
                     break;
-                }else if(tableauMorpion[2][0]==X && tableauMorpion[1][1]==X && tableauMorpion[0][2]==X) {
+                } else if (tableauMorpion[0][1] == X && tableauMorpion[1][1] == X && tableauMorpion[2][1] == X) {
+                    System.out.println(Arrays.deepToString(tableauMorpion));
+                    System.out.println(jUn + " a gagné !");
+                    break;
+                } else if (tableauMorpion[0][2] == X && tableauMorpion[1][2] == X && tableauMorpion[2][2] == X) {
+                    System.out.println(Arrays.deepToString(tableauMorpion));
+                    System.out.println(jUn + " a gagné !");
+                    break;
+                } else if (tableauMorpion[0][0] == X && tableauMorpion[1][1] == X && tableauMorpion[2][2] == X) {
+                    System.out.println(Arrays.deepToString(tableauMorpion));
+                    System.out.println(jUn + " a gagné !");
+                    break;
+                } else if (tableauMorpion[2][0] == X && tableauMorpion[1][1] == X && tableauMorpion[0][2] == X) {
                     System.out.println(Arrays.deepToString(tableauMorpion));
                     System.out.println(jUn + " a gagné !");
                     break;
                 }
                 System.out.println(Arrays.deepToString(tableauMorpion));
-                System.out.println(jDeux+", où voulez vous placer votre O.");
+                System.out.println(jDeux + ", où voulez vous placer votre O.");
                 char reponseJDeux = clavier.nextLine().toUpperCase().charAt(0);
                 switch (reponseJDeux) {
                     case 'A':
@@ -152,44 +154,49 @@ public class Main {
                         nbTours -= 1;
                         break;
                 }
-                if (tableauMorpion[0][0]==O && tableauMorpion[1][0]==O && tableauMorpion[2][0]==O) {
-                    System.out.println(Arrays.deepToString(tableauMorpion));
-                    System.out.println(jDeux+" a gagné !");
-                    break;
-                } else if(tableauMorpion[0][0]==O && tableauMorpion[0][1]==O && tableauMorpion[0][2]==O) {
-                    System.out.println(Arrays.deepToString(tableauMorpion));
-                    System.out.println(jDeux+" a gagné !");
-                    break;
-                }else if(tableauMorpion[1][0]==O && tableauMorpion[1][1]==O && tableauMorpion[1][2]==O) {
-                    System.out.println(Arrays.deepToString(tableauMorpion));
-                    System.out.println(jDeux+" a gagné !");
-                    break;
-                }else if(tableauMorpion[2][0]==O && tableauMorpion[2][1]==O && tableauMorpion[2][2]==O) {
+                if (tableauMorpion[0][0] == O && tableauMorpion[1][0] == O && tableauMorpion[2][0] == O) {
                     System.out.println(Arrays.deepToString(tableauMorpion));
                     System.out.println(jDeux + " a gagné !");
                     break;
-                } else if(tableauMorpion[0][1]==O && tableauMorpion[1][1]==O && tableauMorpion[2][1]==O) {
+                } else if (tableauMorpion[0][0] == O && tableauMorpion[0][1] == O && tableauMorpion[0][2] == O) {
                     System.out.println(Arrays.deepToString(tableauMorpion));
                     System.out.println(jDeux + " a gagné !");
                     break;
-                } else if(tableauMorpion[0][2]==O && tableauMorpion[1][2]==O && tableauMorpion[2][2]==O) {
+                } else if (tableauMorpion[1][0] == O && tableauMorpion[1][1] == O && tableauMorpion[1][2] == O) {
                     System.out.println(Arrays.deepToString(tableauMorpion));
                     System.out.println(jDeux + " a gagné !");
                     break;
-                }else if(tableauMorpion[0][0]==O && tableauMorpion[1][1]==O && tableauMorpion[2][2]==O) {
+                } else if (tableauMorpion[2][0] == O && tableauMorpion[2][1] == O && tableauMorpion[2][2] == O) {
                     System.out.println(Arrays.deepToString(tableauMorpion));
                     System.out.println(jDeux + " a gagné !");
                     break;
-                }else if(tableauMorpion[2][0]==O && tableauMorpion[1][1]==O && tableauMorpion[0][2]==O) {
+                } else if (tableauMorpion[0][1] == O && tableauMorpion[1][1] == O && tableauMorpion[2][1] == O) {
+                    System.out.println(Arrays.deepToString(tableauMorpion));
+                    System.out.println(jDeux + " a gagné !");
+                    break;
+                } else if (tableauMorpion[0][2] == O && tableauMorpion[1][2] == O && tableauMorpion[2][2] == O) {
+                    System.out.println(Arrays.deepToString(tableauMorpion));
+                    System.out.println(jDeux + " a gagné !");
+                    break;
+                } else if (tableauMorpion[0][0] == O && tableauMorpion[1][1] == O && tableauMorpion[2][2] == O) {
+                    System.out.println(Arrays.deepToString(tableauMorpion));
+                    System.out.println(jDeux + " a gagné !");
+                    break;
+                } else if (tableauMorpion[2][0] == O && tableauMorpion[1][1] == O && tableauMorpion[0][2] == O) {
                     System.out.println(Arrays.deepToString(tableauMorpion));
                     System.out.println(jDeux + " a gagné !");
                     break;
                 }
 
-            }if (nbTours==0){
-            System.out.println("C'est un match nul !");
-        }
-        System.out.println("La partie est fini !");
-        }
+            }
+            if (nbTours == 0) {
+                System.out.println("C'est un match nul !");
+            }
+            System.out.println("Voulez-vous rejouer ? (o/n) :");
+            reponseRelancer = clavier.nextLine().toUpperCase();
+        } while (reponseRelancer.equals("O") || reponseRelancer.equals("OUI"));
+        System.out.println("Merci Beaucoup d'avoir joué !");
+        System.out.println("A bientôt !");
+    }
 
     }
