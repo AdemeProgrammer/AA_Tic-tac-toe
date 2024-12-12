fa94e0db4cc93dd69a754992f22ce17b52c3896a
Ademe <A.AYAD@lprs.fr>
2024-12-12 14:32:44 +0100

    [feat] Les joueurs peuvent entrer leur nom

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
         System.out.println("Bienvenue au jeu du morpion (ou tic tac toe ( ou croix rond pour les intimes))");
-        System.out.println("Le joueur 1 devra jouer ");
+        System.out.println("Le joueur 1 devra jouer les 'X' et le joueur 2 les 'O'");
+        System.out.println("Il faut alligner trois mêmes symboles pour gagner la partie");
+        System.out.println("Joueur 1, veuillez entrer votre nom");
+        String jUn = clavier.nextLine();
+        System.out.println("Joueur 2, à votre tour d'entrer votre nom");
+        String jDeux = clavier.nextLine();
+        
     }
 }
